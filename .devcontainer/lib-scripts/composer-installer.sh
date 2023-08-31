0a1d211be2451333d84bb6260349ef1712e992bc
Tristan Elliott <121314838+TheDevStallion96@users.noreply.github.com>
2023-08-31 03:41:36 +0200

    Create composer-installer.sh

---
+++ .devcontainer/lib-scripts/composer-installer.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Download and install Composer
+EXPECTED_SIGNATURE="$(curl -s https://composer.github.io/installer.sig)"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_SIGNATURE="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]; then
+    >&2 echo 'ERROR: Invalid installer signature'
+    rm composer-setup.php
+    exit 1
+fi
+
+php composer-setup.php --quiet
+RESULT=$?
+rm composer-setup.php
+
+# Move Composer executable to a directory in your PATH
+if [ $RESULT -eq 0 ]; then
+    mv composer.phar /usr/local/bin/composer
+    echo "Composer installed successfully!"
+else
+    >&2 echo 'ERROR: Composer installation failed'
+fi
